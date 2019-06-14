fb3cbd176bad108152ea125de7f9cee081cac726
yenight <30335276+yenight@users.noreply.github.com>
2019-06-14 13:46:55 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+var user = {}
+user.name = 'John'
+user.surname = 'Mike'
+console.log(user)
+user.name = 'Peter'
+console.log(user)
+delete user.name
+console.log(user)
+
+var fruit = {
+    apple: 20,
+    pear: 20,
+    peach: 10
+};
+// write your code here...
+console.log(fruit.apple + fruit.pear + fruit.peach)
